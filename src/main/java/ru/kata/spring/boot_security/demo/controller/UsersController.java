a49fd20e6c7d31e5c09bf9bdb5c2cac3ebf1afd3
Никита <godunov987@mail.ru>
2022-08-03 18:11:46 +0300

    complete

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UsersController.java
@@ -0,0 +1,32 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.servise.UserService;
+
+import java.security.Principal;
+
+
+@Controller
+@RequestMapping(value = "/user")
+public class UsersController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping()
+    public String getUserPage(Model model, Principal principal) {
+        User u = (User) userService.loadUserByUsername(principal.getName());
+        model.addAttribute("user", u);
+        return "index";
+    }
+}
